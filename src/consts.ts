92eb167132d5691038e484e719093c982c906921
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-08-11 10:30:26 +0900

    NAV_ITEMSを追加

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,9 @@
-export const SITE_TITLE = "Tomoki Saito's Portfolio"
-export const SITE_DESCRIPTION = '齋藤 智樹のポートフォリオサイトです。'
-export const JOB_TITLE = 'Web Developer & UX Designer'
-export const GITHUB_URL = 'https://github.com/Tomoki-Saito'
+export const SITE_TITLE = "Tomoki Saito's Portfolio";
+export const SITE_DESCRIPTION = "齋藤 智樹のポートフォリオサイトです。";
+export const JOB_TITLE = "Web Developer & UX Designer";
+export const GITHUB_URL = "https://github.com/Tomoki-Saito";
 
 // 環境ごとに異なるので process.env で取得する
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const NAV_ITEMS = ["", "about", "skills", "blog", "contact"];
